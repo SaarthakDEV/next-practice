81a7bd932194fd1107b1c7f85bd2b11a00c065f2
SaarthakDEV <dev.saaarthak@gmail.com>
2025-07-20 20:02:13 +0530

    20 july commit

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
 
 
 export const middleware = (request) => {
@@ -13,3 +15,15 @@ export const middleware = (request) => {
 // export const config = {
 //     matcher: "/profile"
 // }
+
+
+export default clerkMiddleware()
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
